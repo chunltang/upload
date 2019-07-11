f9b0da3b6d95a5fdf60c3ae444e13c0f02f12981
cltang199424 <1471728876@qq.com>
2019-07-11 19:10:21 +0800

    first commit

---
+++ src/main/java/com/example/upload/UploadApplication.java
@@ -0,0 +1,29 @@
+package com.example.upload;
+
+import org.apache.coyote.http11.AbstractHttp11Protocol;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class UploadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UploadApplication.class, args);
+    }
+
+    @Bean
+    public TomcatServletWebServerFactory tomcatEmbedded() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addConnectorCustomizers((TomcatConnectorCustomizer) connector -> {
+            if ((connector.getProtocolHandler() instanceof AbstractHttp11Protocol<?>)) {
+                //-1 means unlimited  不限制上传文件大小
+                ((AbstractHttp11Protocol<?>) connector.getProtocolHandler()).setMaxSwallowSize(-1);
+            }
+        });
+        return tomcat;
+    }
+
+}
